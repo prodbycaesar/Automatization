26b0d3a1afa729574c62ce40096e06d31c671fdf
Julius Caesar <julius@Julius-MacBook-Pro.local>
2024-07-17 08:33:09 +0200

    added kubernetes yml and comments to code

--- help.sh
+++ help.sh
@@ -1,8 +1,12 @@
+# searching in arguments for flags
 hashflag() {
     local flags="$@"
 
+    # scanning script arguments for flags
     for var in $ARGS; do
+        # scanning command if flag is available
         for flag in $flags; do
+            # with a valid flag being used return true
             if [ "$var" = "$flags" ]; then
                 echo 'true'
                 return
@@ -12,12 +16,17 @@ hashflag() {
     echo 'false'
 }
 
+# searching in arguments for values
 readopt() {
     local opts="$@"
 
+    # scanning script arguments for values
     for var in $ARGS; do
+        # scanning command if values are expected
         for opt in $opts; do
+            # check if script arguments and command arguments are the same
             if [[ "$var" = ${opt}* ]]; then
+                # cut away the command and '=' for returning the value only
                 local value="${var//${opt}=/}"
                 if [ "$value" != "$var" ]; then
                     echo $value
@@ -29,8 +38,12 @@ readopt() {
     echo ""
 }
 
+# error checking and output if error occures
 check_error() {
     local msg="$*"
+    
+    # removes word error from string and matches the strings 
+    # if string is not matching the functions has an error and returns the error message
     if [ "${msg//ERROR/}" != "${msg}" ]; then
         echo "${msg}"
         exit 1
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-# Variables
+# variables
 variable "ci_pw" {
         type =  string
         sensitive = true
@@ -19,19 +19,24 @@ variable "vm_count" {
 
 
 
-# Create vms
+# create vms
 resource "proxmox_vm_qemu" "j4s_template" {
     count = var.vm_count
+    # name with deployment name + id
+    # counts up +1 in a multiple deployment scenario
     name = "${var.depl_name}${var.vm_id + count.index}"
     target_node = "caesarlab"
     clone = "templateV3"
     full_clone = true
+    # counts up +1 in a multiple deployment scenario
     vmid = "21${var.vm_id + count.index}"
     os_type = "cloud-init"
     qemu_os = "l26"
     sockets = 1
+    # cpu cores per vm
     cores = 4
     cpu = "host"
+    # ram per vm
     memory = 4096
     scsihw = "virtio-scsi-pci"
     bootdisk = "scsi1"
@@ -40,6 +45,7 @@ resource "proxmox_vm_qemu" "j4s_template" {
         scsi {
             scsi0 {
                 disk {
+                    # disk size per vm
                     size = "32G"
                     storage = "local-lvm"
                 }
@@ -59,14 +65,15 @@ resource "proxmox_vm_qemu" "j4s_template" {
         link_down = false
     }
 
-    # Cloud init
+    # cloud init config
+    # sets root user, ssh keys, network config and dns
     ciuser = "root"
     cipassword = var.ci_pw
     sshkeys = file("/home/j4s/auto/cred/adminvm.pub")
     ipconfig0 = "ip=10.10.21.${var.vm_id + count.index}/16,gw=10.10.0.1"
     nameserver = "10.10.20.5"
 
-    # Set hostname
+    # set hostname
     provisioner "remote-exec" {
         connection {
             timeout     = "1m"
@@ -75,25 +82,30 @@ resource "proxmox_vm_qemu" "j4s_template" {
             private_key = file("/root/.ssh/id_rsa")
             host        = "10.10.21.${var.vm_id + count.index}"
         }
+        # set vm static hostname
         inline = [
             "sudo hostnamectl set-hostname ${var.depl_name}${var.vm_id}"
         ]
     }
 
-    # Ansible script
+    # ansible script
+    # add server to tmp serverfile for deployment
     provisioner "local-exec" {
         command = <<EOT
             echo "[new_servers]" > /tmp/new_hosts
             echo "10.10.21.${var.vm_id + count.index}" >> /tmp/new_hosts
         EOT
     }
-
+    
+    # deploy ansible playbook for inital config and kubernetes config
     provisioner "local-exec" {
         command = <<EOT
             ansible-playbook --vault-password-file /home/j4s/auto/cred/.vault_pass.txt --ssh-extra-args='-o StrictHostKeyChecking=no' -i /tmp/new_hosts /home/j4s/auto/ansible/vm_inital_config.ansible.yml
+            ansible-playbook --ssh-extra-args='-o StrictHostKeyChecking=no' -i /tmp/new_hosts /home/j4s/auto/ansible/ansible/kubernetes_initial_config.ansible.yml
+
         EOT
     }
-
+    # add servers to permanent ansible host file
     provisioner "local-exec" {
         command = <<EOT
             echo "10.10.21.${var.vm_id + count.index}" >> /etc/ansible/hosts
